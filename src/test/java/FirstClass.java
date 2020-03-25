1cedf896fabb04fd52b968bc8fd52a98562421db
edilbekbrowne <58631727+edilbekbrowne@users.noreply.github.com>
2020-03-25 19:08:24 -0400

    Update FirstClass.java

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -2,5 +2,6 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("brave new World");
         System.out.println("someone");
+        System.out.println("edited from GitHub or added");
     }
 }
