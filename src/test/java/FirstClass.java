0ad9b9d2e6f6203f55f00e651eb57df6a88795fa
edilbekbrowne <58631727+edilbekbrowne@users.noreply.github.com>
2020-03-25 19:58:09 -0400

    Update FirstClass.java

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -3,8 +3,7 @@ public class FirstClass {
         System.out.println("brave new World");
         System.out.println("someone"); //comment 
         System.out.println("change one");
-        System.out.println("change two"); // comment
-
+        System.out.println("change two");
         System.out.println("change three");
         //first change
         //another change
