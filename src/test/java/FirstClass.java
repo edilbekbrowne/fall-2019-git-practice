5dc612991b29d8ff502f5609b0ce2bdf2c1ef15f
Yourname <YourEmail>
2020-03-25 20:21:13 -0400

    updated version of FirstClass

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -2,6 +2,8 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("brave new World");
         System.out.println("someone"); //comment 2
+        System.out.println("brave new World"); // comment
+        System.out.println("someone"); //comment 
         System.out.println("change one");
         System.out.println("change two");
         System.out.println("change three");
